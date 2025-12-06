69019f74ade125a44936c01a35888811f6d2733d
Veronicasr2411 <72567212@mail.isil.pe>
2025-12-06 15:48:46 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $(".slider").bxSlider({
+        mode: 'fade',
+        auto: true,
+        pause: 3000,
+        controls: false,
+        pager: false
+    });
+});
